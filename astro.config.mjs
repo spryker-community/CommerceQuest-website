fc669ffed0461c89d9ad466a27b817c1b92e0bb6
Florian Schwarzmeier <Florian.Schwarzmeier@spryker.com>
2024-08-08 16:56:47 +0200

    Adds basic mdx support to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
 import { fileURLToPath } from 'node:url';
+import mdx from '@astrojs/mdx';
 import netlify from "@astrojs/netlify";
 
 import react from "@astrojs/react";
@@ -18,82 +19,91 @@ export default defineConfig({
     domains: ["images.unsplash.com"]
   },
   prefetch: true,
-  integrations: [tailwind(), sitemap(), starlight({
-    title: "CommerceQuest Docs",
-    editLink: {
-      baseUrl: 'https://github.com/spryker-community/spryker-community.github.io/edit/main'
-    },
-    logo: {
-      src: './src/images/starlight/CQ_logo.svg',
-      replacesTitle: true
-    },
-    defaultLocale: "root",
-    locales: {
-      root: {
-        label: "English",
-        lang: "en"
+  integrations: [
+    tailwind(),
+    sitemap(),
+    starlight({
+      title: "CommerceQuest Docs",
+      editLink: {
+        baseUrl: 'https://github.com/spryker-community/spryker-community.github.io/edit/main'
+      },
+      logo: {
+        src: './src/images/starlight/CQ_logo.svg',
+        replacesTitle: true
+      },
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "English",
+          lang: "en"
+        }
+      },
+      lastUpdated: true,
+      // https://starlight.astro.build/guides/sidebar/
+      sidebar: [{
+        label: "Community Guides",
+        autogenerate: {
+          directory: "guides"
+        }
+      }, {
+        label: "Community Projects",
+        autogenerate: {
+          directory: "tools"
+        }
+      }, {
+        label: "Contributing",
+        autogenerate: {
+          directory: "contributing"
+        }
+      }, {
+        label: "Event Blueprints",
+        autogenerate: {
+          directory: "events"
+        }
+      }, {
+        label: "Other Resources",
+        autogenerate: {
+          directory: "other"
+        }
       }
-    },
-    lastUpdated: true,
-    // https://starlight.astro.build/guides/sidebar/
-    sidebar: [{
-      label: "Community Guides",
-      autogenerate: {
-        directory: "guides"
-      }
-    }, {
-      label: "Community Projects",
-      autogenerate: {
-        directory: "tools"
-      }
-    }, {
-      label: "Contributing",
-      autogenerate: {
-        directory: "contributing"
-      }
-    }, {
-      label: "Event Blueprints",
-      autogenerate: {
-        directory: "events"
-      }
-    }, {
-      label: "Other Resources",
-      autogenerate: {
-        directory: "other"
-      }
-    }
-    /*         {
-              label: "[Templates]]",
-              autogenerate: { directory: "templates" },
-            }, */],
-    social: {
-      github: "https://github.com/spryker-community"
-    },
-    disable404Route: true,
-    customCss: ["./src/assets/styles/starlight.css"],
-    favicon: "/favicon.ico",
-    components: {
-      SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
-      Head: "./src/components/ui/starlight/Head.astro",
-      Page: "./src/components/ui/starlight/Page.astro"
-    },
-    head: [{
-      tag: "meta",
-      attrs: {
-        property: "og:image",
-        content: "https://commercequest.space/" + "/social.webp"
-      }
-    }, {
-      tag: "meta",
-      attrs: {
-        property: "twitter:image",
-        content: "https://commercequest.space/" + "/social.webp"
-      }
-    }]
-  }), compressor({
-    gzip: false,
-    brotli: true
-  }), react(), markdoc(), keystatic()],
+      /*         {
+                label: "[Templates]]",
+                autogenerate: { directory: "templates" },
+              }, */],
+      social: {
+        github: "https://github.com/spryker-community"
+      },
+      disable404Route: true,
+      customCss: ["./src/assets/styles/starlight.css"],
+      favicon: "/favicon.ico",
+      components: {
+        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
+        Head: "./src/components/ui/starlight/Head.astro",
+        Page: "./src/components/ui/starlight/Page.astro"
+      },
+      head: [{
+        tag: "meta",
+        attrs: {
+          property: "og:image",
+          content: "https://commercequest.space/" + "/social.webp"
+        }
+      }, {
+        tag: "meta",
+        attrs: {
+          property: "twitter:image",
+          content: "https://commercequest.space/" + "/social.webp"
+        }
+      }]
+    }),
+    compressor({
+      gzip: false,
+      brotli: true
+    }),
+    react(),
+    markdoc(),
+    mdx(),
+    keystatic()
+  ],
   output: "hybrid",
   experimental: {
     clientPrerender: true,
