2dce8270657b817ed2006e1ca90615bcdfc51c06
Guido X Jansen <x@gui.do>
2024-11-27 17:08:24 +0100

    redirect update

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,8 +1,6 @@
 import { makeHandler } from '@keystatic/astro/api';
 import keystaticConfig from '../../../../keystatic.config';
 
-export const prerender = false;
-
 export const all = makeHandler({
   config: keystaticConfig,
 });
